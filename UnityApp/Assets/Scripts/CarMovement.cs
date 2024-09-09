aeb52faa568b30bb534e8753bc1771ce0b22b5e7
golosoman <constan.lapin2015@mail.ru>
2024-09-09 18:29:02 +0400

    edit points for T-intersection map, map has been expanded for T and Cross Intersection, unnecessary materials have been removed and edit CarMovement script such as added turns

--- UnityApp/Assets/Scripts/CarMovement.cs
+++ UnityApp/Assets/Scripts/CarMovement.cs
@@ -7,20 +7,19 @@ public class CarMovement : MonoBehaviour
     public Transform[] route;
     private int currentPoint = 0;
     public float speed = 5f;
+    public float rotationSpeed = 5f;
     private bool isMoving = false;
 
     public bool IsMoving => isMoving;
 
     public void SetRoute(Transform[] route, float speed)
     {
-        // Debug.Log(route.Length);
         this.route = route;
         this.speed = speed;
     }
 
     public void StartMovement()
     {
-        // Debug.Log(route.Length);
         if (!isMoving && route != null && route.Length > 0)
         {
             StartCoroutine(MoveAlongRoute());
@@ -52,9 +51,25 @@ public class CarMovement : MonoBehaviour
         while (isMoving && currentPoint < route.Length)
         {
             Vector3 targetPosition = route[currentPoint].position;
+            
+            // Получаем направление к следующей точке
+            Vector3 direction = targetPosition - transform.position;
+
+            // Рассчитываем угол поворота только по оси Z
+            float targetAngleZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Получаем текущий угол вращения
+            Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngleZ+90);
+
+            // Пока автомобиль не достиг следующей точки
             while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
             {
+                // Плавно поворачиваем автомобиль по оси Z
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                
+                // Перемещаем автомобиль к следующей точке
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
                 yield return null;
             }
 
