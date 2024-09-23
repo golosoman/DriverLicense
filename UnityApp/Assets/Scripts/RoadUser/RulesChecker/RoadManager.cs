c2cae1f9e61b4657f060c784efcd910119b2ffdc
golosoman <constan.lapin2015@mail.ru>
2024-09-23 22:46:29 +0400

    New scenes have been added, and elements for the user interface have been added.

--- UnityApp/Assets/Scripts/RoadUser/RulesChecker/RoadManager.cs
+++ UnityApp/Assets/Scripts/RoadUser/RulesChecker/RoadManager.cs
@@ -13,26 +13,14 @@ public class RoadManager : MonoBehaviour
     public Dictionary<string, GameObject> RoadUserSpawnPoints { get => roadUserSpawnPoints; }
     public TrafficLightData[] TrafficLightDatas { get => trafficLightDatas; }
     public SignData[] SignDatas { get => signDatas; }
-    public bool ViolationRules {get; set;}
-    // public Dictionary<string, Dictionary<RoadUserData, GameObject>> RoadUsers { get => roadUsers; set => roadUsers = value; }
+    public bool ViolationRules { get; set; }
 
-    public void Initialize(string typeIntersection, TrafficLightData[] trafficLightDatas, SignData[] signDatas, Dictionary<string, GameObject> roadUserSpawnPoints){
-        // roadUsers = new Dictionary<string, Dictionary<RoadUserData, GameObject>>();
-        // // Инициализация ключей
-        // roadUsers[RoadUserTypes.CAR] = new Dictionary<RoadUserData, GameObject>();
-        // roadUsers[RoadUserTypes.HUMAN] = new Dictionary<RoadUserData, GameObject>();
-        // roadUsers[RoadUserTypes.TRAM] = new Dictionary<RoadUserData, GameObject>();
+    public void Initialize(string typeIntersection, TrafficLightData[] trafficLightDatas, SignData[] signDatas, Dictionary<string, GameObject> roadUserSpawnPoints)
+    {
         this.typeIntersection = typeIntersection;
         this.trafficLightDatas = trafficLightDatas;
         this.signDatas = signDatas;
         this.roadUserSpawnPoints = roadUserSpawnPoints;
         ViolationRules = false;
     }
-
-    // public void AddRoadUser(RoadUserData newRoadUserData, GameObject newRoadUserObject){
-    // if (!roadUsers.ContainsKey(newRoadUserData.TypeParticipant)) {
-    //     roadUsers[newRoadUserData.TypeParticipant] = new Dictionary<RoadUserData, GameObject>();
-    // }
-    // roadUsers[newRoadUserData.TypeParticipant].Add(newRoadUserData, newRoadUserObject);
-    // }
 }
